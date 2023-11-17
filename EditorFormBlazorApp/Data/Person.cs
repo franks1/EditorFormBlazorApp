da3fbef308e91d12dec648ca4b57ed4541b4ead5
Francis Banini <fkbani@gmail.com>
2023-11-17 08:25:54 +0000

    add " @rendermode InteractiveServer " correct form not rendering

--- EditorFormBlazorApp/Data/Person.cs
+++ EditorFormBlazorApp/Data/Person.cs
@@ -1,7 +1,11 @@
-﻿namespace EditorFormBlazorApp.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EditorFormBlazorApp.Data;
 public class Person
 {
+    [Required]
     public string username { get; set; }
+    [Required]
     public string address { get; set; }
 }
 
